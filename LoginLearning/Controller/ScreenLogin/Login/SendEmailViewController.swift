91211cc9577b4ce2e6cb6d1794310e0fb9bb67e7
noerindo <indahnoerindo@gmail.com>
2023-08-28 09:09:22 +0700

    send verifikasi account done

--- LoginLearning/Controller/ScreenLogin/Login/SendEmailViewController.swift
+++ LoginLearning/Controller/ScreenLogin/Login/SendEmailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class SendEmailViewController: UIViewController {
-
+    private let loginVM = LoginViewModel()
     @IBOutlet weak var emailTextInput: UITextField! {
     didSet {
         emailTextInput.addShadow(color: .gray, width: 0.5, text: emailTextInput)
@@ -28,6 +28,27 @@ class SendEmailViewController: UIViewController {
     
 
     @IBAction func sendActionBtn(_ sender: UIButton) {
+        guard let email = emailTextInput.text else {return}
+         
+        if AcountRegis.invalidEmail(email: email) {
+            loginVM.postVerifikasiAccount(email: email) { result in
+                DispatchQueue.main.async {
+                    let refreshAlert = UIAlertController(title: "Succes", message: result, preferredStyle: UIAlertController.Style.alert)
+
+                    refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+                        let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+                        self.navigationController?.pushViewController(loginVC, animated: true)
+                    }))
+
+                    self.present(refreshAlert, animated: true, completion: nil)
+                }
+            } onError: { error in
+                SnackBarWarning.make(in: self.view, message: error, duration: .lengthShort).show()
+            }
+
+        } else {
+            SnackBarWarning.make(in: self.view, message: "Email is not valid.", duration: .lengthShort).show()
+        }
         
     }
     
